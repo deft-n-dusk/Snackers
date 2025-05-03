b7f69c0088490188716c3a37d47ee4923737555a
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-05-03 17:46:37 +0530

    Fixed CORS

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,6 +1,6 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const REST_API="https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6691565&lng=77.45375779999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const REST_API="https://snackersbe.netlify.app/.netlify/functions/restaurants"
 
 
 export const MENU_API="https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6691565&lng=77.45375779999999&restaurantId="
